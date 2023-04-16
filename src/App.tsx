f1e5d4dda683c20e2c5de34b0a8142bddc53cb17
coolweb131 <timewornbn@gmail.com>
2023-04-16 16:30:49 -0300

    chore: fix router

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/room' element={<Room />} />
+        <Route path='/room/*' element={<Room />} />
       </Routes>
     </BrowserRouter>
   );

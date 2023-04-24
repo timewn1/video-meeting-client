cc897e932d2b23cb6781cf1261689d46eb8c4a56
coolweb131 <timewornbn@gmail.com>
2023-04-24 12:37:44 -0300

    chore: update server url

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 // const SERVER_URL = 'http://localhost:3001';
 
-const SERVER_URL = 'https://video-meeting.deamtest.com/room/#efd6-a2da-4c2b-9a0e';
+const SERVER_URL = 'https://video-meeting.deamtest.com';
 
 export {
     SERVER_URL
